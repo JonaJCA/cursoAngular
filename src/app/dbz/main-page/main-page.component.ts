c29408bcefd13499dcb55dd3e4141de0c2b53fd9
Jonathan <jonathan_corro@hotmail.com>
2023-01-18 01:33:20 -0500

    segundo avance de bases Angular

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 16000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = 
+    {
+      nombre: 'Maestro Rochi',
+      poder: 1000
+    }
+  
+
+ 
+}
